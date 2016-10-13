33bc23f290fbdd9098703385ba9617939d8a146e
neptunian0 <the-neptunian@hotmail.com>
2016-10-13 17:39:04 +0100

    Applied Material Design Collapsing Toolbar Layout to the application - simply reused the Pluralsight module material

--- app/src/main/java/com/lloydtucker/bluebankv2/MainActivity.java
+++ app/src/main/java/com/lloydtucker/bluebankv2/MainActivity.java
@@ -1,8 +1,8 @@
 package com.lloydtucker.bluebankv2;
 
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 import com.lloydtucker.bluebankv2.helpers.BlueBankApiAdapter;
 import com.lloydtucker.bluebankv2.interfaces.ApiAdapter;
@@ -11,10 +11,17 @@ import com.lloydtucker.bluebankv2.pojos.Customers;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Calendar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 import static com.lloydtucker.bluebankv2.helpers.Constants.numberApis;
 
 public class MainActivity extends AppCompatActivity {
+//    @BindView(R.id.toolbar) Toolbar toolbar;
+    @BindView(R.id.collapsingToolbar) CollapsingToolbarLayout collapsingToolbar;
+
     ArrayList<Customers> customers = new ArrayList<>();
     ArrayList<Accounts> accounts = new ArrayList<>();
     String result = "";
@@ -23,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
         ApiAdapter[] apiAdapter = new ApiAdapter[numberApis];
         apiAdapter[0] = new BlueBankApiAdapter();
@@ -43,9 +51,25 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
-        //test it worked
-        Log.d("MainActivity", customers.get(0).getGivenName());
-        Log.d("MainActivity", accounts.get(0).getAccountNumber());
-        Log.d("MainActivity", accounts.get(1).getAccountNumber());
+        //set the toolbar to include the greeting and the customer name
+        String greeting = getGreeting() + customers.get(0).getGivenName();
+        collapsingToolbar.setTitle(greeting);
+    }
+
+    //Determine which greeting to display in the toolbar
+    public String getGreeting() {
+        //set the greeting text to good morning, good afternoon,
+        //or good evening <name> and the date
+        String greeting = "";
+        Calendar c = Calendar.getInstance();
+        int timeOfDay = c.get(Calendar.HOUR_OF_DAY);
+        if (timeOfDay >= 0 && timeOfDay < 12) {
+            greeting += "Good morning, ";
+        } else if (timeOfDay >= 12 && timeOfDay < 17) {
+            greeting += "Good afternoon, ";
+        } else if (timeOfDay >= 17 && timeOfDay < 24) {
+            greeting += "Good evening, ";
+        }
+        return greeting;
     }
 }
