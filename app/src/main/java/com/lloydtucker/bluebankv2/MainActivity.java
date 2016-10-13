4c95c8d9afce8aa5a2a5d15dd41c46d60487c7a1
neptunian0 <the-neptunian@hotmail.com>
2016-10-13 16:56:32 +0100

    Retrieving BlueBank accounts successfully

--- app/src/main/java/com/lloydtucker/bluebankv2/MainActivity.java
+++ app/src/main/java/com/lloydtucker/bluebankv2/MainActivity.java
@@ -2,15 +2,21 @@ package com.lloydtucker.bluebankv2;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.lloydtucker.bluebankv2.helpers.BlueBankApiAdapter;
 import com.lloydtucker.bluebankv2.interfaces.ApiAdapter;
+import com.lloydtucker.bluebankv2.pojos.Accounts;
+import com.lloydtucker.bluebankv2.pojos.Customers;
 
 import java.io.IOException;
 import java.util.ArrayList;
 
+import static com.lloydtucker.bluebankv2.helpers.Constants.numberApis;
+
 public class MainActivity extends AppCompatActivity {
     ArrayList<Customers> customers = new ArrayList<>();
+    ArrayList<Accounts> accounts = new ArrayList<>();
     String result = "";
 
     @Override
@@ -18,11 +24,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ApiAdapter apiAdapter = new BlueBankApiAdapter();
+        ApiAdapter[] apiAdapter = new ApiAdapter[numberApis];
+        apiAdapter[0] = new BlueBankApiAdapter();
+
+        //Retrieve the customer data
         try{
-            apiAdapter.getCustomers();
+            ArrayList<Customers> temp = apiAdapter[0].getCustomers();
+            customers.addAll(temp);
         } catch(IOException e){
             e.printStackTrace();
         }
+
+        //Retrieve the account data
+        try{
+            ArrayList<Accounts> temp = apiAdapter[0].getAccounts();
+            accounts.addAll(temp);
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+
+        //test it worked
+        Log.d("MainActivity", customers.get(0).getGivenName());
+        Log.d("MainActivity", accounts.get(0).getAccountNumber());
+        Log.d("MainActivity", accounts.get(1).getAccountNumber());
     }
 }
