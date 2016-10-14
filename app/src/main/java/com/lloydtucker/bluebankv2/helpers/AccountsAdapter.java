df3e39a4d598140d313807ed86b5e977034e0f20
neptunian0 <the-neptunian@hotmail.com>
2016-10-14 18:05:22 +0100

    Finished getting the AccountAdapter to work. Bank details and balances are now showing.

--- app/src/main/java/com/lloydtucker/bluebankv2/helpers/AccountsAdapter.java
+++ app/src/main/java/com/lloydtucker/bluebankv2/helpers/AccountsAdapter.java
@@ -6,11 +6,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.lloydtucker.bluebankv2.R;
 import com.lloydtucker.bluebankv2.pojos.Accounts;
 
+import java.text.DecimalFormat;
 import java.util.List;
 
 import butterknife.BindView;
@@ -38,7 +40,7 @@ public class AccountsAdapter extends RecyclerView.Adapter<AccountsAdapter.MyView
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         Log.d(TAG, "onCreateViewHolder");
-        View view = mInflater.inflate(R.layout.scroll_view_item, parent, false);
+        View view = mInflater.inflate(R.layout.recycler_view_item, parent, false);
         return new MyViewHolder(view);
     }
 
@@ -57,32 +59,100 @@ public class AccountsAdapter extends RecyclerView.Adapter<AccountsAdapter.MyView
     }
 
     class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
-        @BindView(R.id.accountHeader) TextView accountHeader;
-        @BindView(R.id.accountBody) TextView accountBody;
+        @BindView(R.id.accountBalance)  TextView accountBalance;
+        @BindView(R.id.accountDetails)  TextView accountDetails;
+        @BindView(R.id.accountType)     TextView accountType;
+        @BindView(R.id.accountImage)    ImageView accountImage;
         int position;
         Accounts current;
-
-        void setData(Accounts current, int position) {
-            this.accountHeader.setText(current.getAccountFriendlyName());
-            this.accountBody.setText(current.getAccountNumber());
-            this.position = position;
-            this.current = current;
-        }
 
         MyViewHolder(View itemView){
             super(itemView);
             ButterKnife.bind(this, itemView);
         }
 
-        @Override
-        public void onClick(View v) {
+        void setData(Accounts current, int position) {
+            String accountType =    current.getAccountFriendlyName();
+            String accountDetails = current.getAccountNumber() + "   |   " +
+                                    formatSortCode(current.getSortCode());
+            String accountBalance = getCurrencySymbol(current.getAccountCurrency()) +
+                                    formatBalance(current.getAccountBalance());
+            int accountImage =      getImage(accountType);
 
+            this.accountType.setText(accountType);
+            this.accountDetails.setText(accountDetails);
+            this.accountBalance.setText(accountBalance);
+            this.accountImage.setImageResource(accountImage);
+            this.position = position;
+            this.current = current;
         }
+
+        @Override
+        public void onClick(View v) {}
 
         void setListeners(){
 //            imgDelete.setOnClickListener(MyViewHolder.this);
 //            imgAdd.setOnClickListener(MyViewHolder.this);
 //            imgThumb.setOnClickListener(MyViewHolder.this);
+        }
+
+        private int getImage(String accountType){
+            int drawableSource;
+            switch(accountType){
+                case "Current Account":
+                    drawableSource = R.mipmap.ic_payment_white_48dp;
+                    break;
+                case "Savings Account":
+                    drawableSource = R.mipmap.ic_save_white_48dp;
+                    break;
+                case "John Smith - Current Account":
+                    drawableSource = R.mipmap.ic_monetization_on_white_48dp;
+                    break;
+                default:
+                    throw new IllegalArgumentException("Unsupported account type: "
+                            + accountType);
+            }
+            return drawableSource;
+        }
+
+        private String formatBalance(Double balance){
+            DecimalFormat formatter = new DecimalFormat("#,##0.00");
+            return formatter.format(balance);
+        }
+
+        private String formatSortCode(String sortCode){
+            String formatted = "";
+            if(sortCode.length() % 2 != 0){
+                throw new IllegalArgumentException("Invalid Sort Code: "
+                        + sortCode);
+            }
+            for(int i = 0; i < sortCode.length(); i+=2){
+                formatted += sortCode.charAt(i);
+                formatted += sortCode.charAt(i + 1);
+                if(i+2 < sortCode.length()){
+                    formatted += "\u2013";
+                }
+            }
+            return formatted;
+        }
+
+        private String getCurrencySymbol(String currency){
+            String symbol;
+            switch(currency){
+                case "GBP":
+                    symbol = "£";
+                    break;
+                case "EUR":
+                    symbol = "€";
+                    break;
+                case "CAD":
+                case "USD":
+                    symbol = "$";
+                    break;
+                default:
+                    throw new IllegalArgumentException("Unsupported currency: " + currency);
+            }
+            return symbol;
         }
     }
 }
